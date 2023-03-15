b9b6b62b529263310c1470d5d39e37f794e6e883
Nerro <nduguiskarioti@gmail.com>
2023-03-15 14:18:54 +0300

    UI for mixmeet app done

---
+++ app.js
@@ -0,0 +1,27 @@
+// const express = require("express")
+const mysql = require("mysql")
+// const app = express()
+const express = require("express")
+
+
+const app = express()
+require("./services/main")
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+app.get("/sign-up", (req,res)=>{
+    res.render("sign-up")
+})
+app.get("/sign-in", (req,res)=>{
+    res.render("sign-in")
+})
+app.listen(3001, ()=>{
+    console.log("app running!!...")
+})
+
+// create a db called mixmeet
+// have a table called users(user_id)
